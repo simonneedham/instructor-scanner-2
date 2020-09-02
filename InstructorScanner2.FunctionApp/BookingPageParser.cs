ad92be59d7b01b11524b2561601dc072e87cb821
simonneedham <1652096+simonneedham@users.noreply.github.com>
2020-09-02 04:16:15 +0100

    Captured instructor json. Captured booking events for time period.

--- InstructorScanner2.FunctionApp/BookingPageParser.cs
+++ InstructorScanner2.FunctionApp/BookingPageParser.cs
@@ -19,20 +19,35 @@ namespace InstructorScanner2.FunctionApp
         private HttpClientHandler _httpClientHandler;
         private HttpClient _httpClient;
         private readonly Uri _rootUrl;
-        private Dictionary<int, string> _instructorDictionary;
-
+        private Dictionary<string, string> _instructorDictionary;
 
         public BookingPageParser(IOptions<AppSettings> appSettingsOptions, ILogger logger)
         {
             _appSettings = appSettingsOptions.Value;
             _logger = logger;
             _rootUrl = new Uri(appSettingsOptions.Value.RootUrl);
+            _instructorDictionary = new Dictionary<string, string>();
         }
 
         public async Task<IList<CalendarDay>> GetBookings(DateTime startDate, DateTime endDate, IList<Instructor> instructors)
         {
             if (_httpClient == null)
                 await InitHttpClientAsync();
+
+            // Get Instructors
+            await UpdateInstructorsAsync(startDate, endDate);
+
+            // Get Bookings
+            var bookingsUrl = _appSettings
+                .BookingsEndpoint
+                .Replace("[start-date]", $"{startDate:ddd MMM dd yyyy} 00:00:00 GMT+0100 (British Summer Time)")
+                .Replace("[end-date]", $"{endDate:ddd MMM dd yyyy} 00:00:00 GMT+0100 (British Summer Time)");
+
+            var bookingsResponse = await _httpClient.PostAsync(new Uri(_rootUrl, bookingsUrl), null);
+            bookingsResponse.EnsureSuccessStatusCode();
+            var bookingsJson = await JsonSerializer.DeserializeAsync<IList<InstructorEventJson>>(await bookingsResponse.Content.ReadAsStreamAsync(), new JsonSerializerOptions { IgnoreNullValues = true, PropertyNameCaseInsensitive = true });
+            var bookingsWithInstructors = bookingsJson.Where(bj => !string.IsNullOrEmpty(bj.resourceId)).ToList();
+
 
             //var bookingPageUrl = new Uri(_rootUrl, $"{_appSettings.BookingPage}?dt={date.ToString("dd/MM/yyyy")}");
             //var bookingPageResponse = await _httpClient.GetAsync(bookingPageUrl);
@@ -103,6 +118,27 @@ namespace InstructorScanner2.FunctionApp
             return new List<CalendarDay>();
         }
 
+        private async Task UpdateInstructorsAsync(DateTime startDate, DateTime endDate)
+        {
+            var date = startDate.Date;
+            while(date <= endDate)
+            {
+                await UpdateInstructorsForDateAsync(date);
+                date = date.AddDays(1);
+                Task.Delay(_appSettings.DelaySecondsPerScan * 1000).Wait();
+            }
+        }
+
+        private async Task UpdateInstructorsForDateAsync(DateTime bookingDate)
+        {
+            var instructorsResponse = await _httpClient.PostAsync(new Uri(_rootUrl, _appSettings.InstructorsEndpoint), new FormUrlEncodedContent(new Dictionary<string, string>() { { "start_date", $"{bookingDate:ddd MMM dd yyyy} 00:00:00 GMT+0100 (British Summer Time)" } }));
+            instructorsResponse.EnsureSuccessStatusCode();
+            var instructorsJson = await JsonSerializer.DeserializeAsync<IList<InstructorJson>>(await instructorsResponse.Content.ReadAsStreamAsync(), new JsonSerializerOptions { IgnoreNullValues = true, PropertyNameCaseInsensitive = true });
+
+            foreach (var i in instructorsJson)
+                _instructorDictionary[i.id] = i.title;
+        }
+
         private async Task InitHttpClientAsync()
         {
             _cookies = new CookieContainer();
@@ -116,11 +152,6 @@ namespace InstructorScanner2.FunctionApp
             var loginPostUrl = new Uri(_rootUrl, _appSettings.LoginPostEndpoint);
             var loginPageResponse = await _httpClient.PostAsync(loginPostUrl, new FormUrlEncodedContent(dictionaryLoginInputs));
             loginPageResponse.EnsureSuccessStatusCode();
-
-            var instructorsResponse = await _httpClient.PostAsync(new Uri(_rootUrl, _appSettings.InstructorsEndpoint), null);
-            instructorsResponse.EnsureSuccessStatusCode();
-            var instructors = await JsonSerializer.DeserializeAsync<IList<InstructorJsonResponse>>(await instructorsResponse.Content.ReadAsStreamAsync(), new JsonSerializerOptions { IgnoreNullValues = true, PropertyNameCaseInsensitive = true });
-            _instructorDictionary = instructors.ToDictionary(i => i.id, i => i.title);
         }
 
         private Dictionary<string, string> GetDictionaryLoginInputs(string loginPageHtml)
@@ -181,12 +212,22 @@ namespace InstructorScanner2.FunctionApp
         }
         #endregion
 
-        private class InstructorJsonResponse
+        private class InstructorJson
         {
-            public int id { get; set; }
+            public string id { get; set; }
             public string title { get; set; }
             public string backgorund_colour { get; set; }
             public string colour { get; set; }
+        }
+
+        private class InstructorEventJson
+        {
+            public bool allDay { get; set; }
+            public string end { get; set; } //2020-09-07 11:00:00
+            public string id { get; set; }
+            public string resourceId { get; set; }
+            public string start { get; set; } //2020-09-07 09:00:00
+            public string title { get; set; }
         }
     }
 }
